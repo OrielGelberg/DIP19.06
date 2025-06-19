48a750baea5aa38bdd15d29a56c4e4fe93cda47a
OrielGelberg <g9093587@gmail.com>
2025-06-19 15:55:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP19._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[FILE] {message}");
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[CONSOLE] {message}");
+        }
+    }
+
+    public class MissionController
+    {
+        private readonly ILogger logger;
+
+        public MissionController(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void RunMission()
+        {
+            Console.WriteLine("Running mission...");
+            logger.Log("Mission completed.");
+        }
+    }
+}
