57a8c1d299a172a0cecd89fc93ecb8f4b3dc69f0
OrielGelberg <g9093587@gmail.com>
2025-06-19 16:32:21 +0300

    1 2 3

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace DIP19._06
         {
         }
     }
+    // Example 1: Dependency Inversion Principle (DIP) in C#
     public interface ILogger
     {
         void Log(string message);
@@ -46,6 +47,82 @@ namespace DIP19._06
         {
             Console.WriteLine("Running mission...");
             logger.Log("Mission completed.");
+        }
+    }
+
+    // Exaple 2
+    public interface ILieDetector
+    {
+        bool DetectLie(string answer);
+    }
+
+    public class VoiceAnalyzer : ILieDetector
+    {
+        public bool DetectLie(string answer)
+        {
+            return answer.Contains("uh");
+        }
+    }
+
+    public class ThermalScanner : ILieDetector
+    {
+        public bool DetectLie(string answer)
+        {
+            return false;
+        }
+    }
+
+    public class TerroristInterrogationUnit
+    {
+        private readonly ILieDetector detector;
+
+        public TerroristInterrogationUnit(ILieDetector detector)
+        {
+            this.detector = detector;
+        }
+
+        public void Interrogate(string answer)
+        {
+            if (detector.DetectLie(answer))
+                Console.WriteLine("Lie detected!");
+            else
+                Console.WriteLine("Truth detected.");
+        }
+    }
+    // Example 3: Dependency Inversion Principle (DIP) in C#
+    public interface IAlert
+    {
+        void Alert();
+    }
+
+    public class SirenAlert : IAlert
+    {
+        public void Alert()
+        {
+            Console.WriteLine("Siren activated!");
+        }
+    }
+
+    public class DroneDispatchAlert : IAlert
+    {
+        public void Alert()
+        {
+            Console.WriteLine("Drone dispatched!");
+        }
+    }
+
+    public class BaseDefenseController
+    {
+        private readonly IAlert alert;
+
+        public BaseDefenseController(IAlert alert)
+        {
+            this.alert = alert;
+        }
+
+        public void HandleThreat()
+        {
+            alert.Alert();
         }
     }
 }
